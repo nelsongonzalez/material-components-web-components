748f0183761a58aed588bf7e5b4dad5c6507ea09
Nelson González <nelzonleonardo@gmail.com>
2021-05-27 23:47:18 -0500

    Changing number of columns across breakpoints

--- packages/image-list/mwc-image-list-base.ts
+++ packages/image-list/mwc-image-list-base.ts
@@ -4,7 +4,10 @@ export type ImageListColumns = 6|5|4|3|2|1;
 
 /** @soyCompatible */
 export class ImageListBase extends LitElement {
-  @property({type: Number}) columns: ImageListColumns = 4;
+  // @property({type: Number}) columns: ImageListColumns = 4;
+  @property({type: Number}) desktop: ImageListColumns = 4;
+  @property({type: Number}) tablet: ImageListColumns = 3;
+  @property({type: Number}) phone: ImageListColumns = 2;
 
   render() {
     return html`<slot></slot>`;
